b4140c5de1e2a56c060df7c5e3eb3f5786f09f40
Qreamville <Oyeleyebalikis@yahoo.com>
2022-10-25 22:51:12 -1200

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+import ProfileImage from "../assets/image/profile__img.svg";
+import { RiShareForwardLine } from "react-icons/ri";
+import { BsThreeDots } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <header className="h-60 flex flex-col items-center justify-center mt-8 relative">
+      <div className="flex flex-col">
+        <Image src={ProfileImage} />
+        <h2 className="text-[#101828] font-semibold text-xl tracking-wider">
+          Annette Black
+        </h2>
+      </div>
+      <div className="absolute top-0 right-10 md:right-40 xl:right-80 border h-10 w-10 grid place-items-center border-dashed border-[#d0d5dd] rounded-full cursor-pointer">
+        <RiShareForwardLine
+          size={20}
+          color="#98A2B3"
+          className="hidden md:block"
+        />
+        <BsThreeDots size={20} color="#98A2B3" className="md:hidden block" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
